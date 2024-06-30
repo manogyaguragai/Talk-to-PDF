94424d1a323111ac54b41c9a969b301ad528f2f0
Manogya Guragai <manogyaguragai24@gmail.com>
2024-06-30 14:51:34 +0545

    implemented RAG, chose a better model that answers better and added PDF validation

--- talk_to_pdf.py
+++ talk_to_pdf.py
@@ -6,7 +6,9 @@ import google.generativeai as genai
 
 import streamlit as st
 
-from PyPDF2 import PdfReader
+import PyPDF2
+
+import pathlib
 
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -23,7 +25,7 @@ def get_pdf_text(pdf_docs):
     text = ""
     # iterate over all pdf files uploaded
     for pdf in pdf_docs:
-        pdf_reader = PdfReader(pdf)
+        pdf_reader = PyPDF2.PdfReader(pdf)
         # iterate over all pages in a pdf
         for page in pdf_reader.pages:
             text += page.extract_text()
@@ -35,33 +37,37 @@ def get_text_chunks(text):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap = 100)
     # now split the text we have using object created
     chunks = text_splitter.split_text(text)
-
     return chunks
 
 # Function that will create a place locally to store the embeddings as a vector quantity 
 def get_vector_store(text_chunks, allow_dangerous_deserialization=True):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/text-embedding-004") 
     vector_store = FAISS.from_texts(text_chunks,embeddings) 
+    #vector_store = FAISS.from_documents(text_chunks, embeddings)
     vector_store.save_local("faiss_index")  
+    return vector_store
 
 # FUnction that makes a conversation chain with a predefined prompt, a selected model, and user input
-def get_conversation_chain():
+def get_conversation_chain(retr):
     # define the prompt
     prompt_template = """
-    You are a capable AI model that answers the question as detailed as possible from the provided context. make sure to provide all the details, if the answer is not in
-    provided context just tell the user that you could not find the relevant answer in the given context, don't provide the wrong answer.\n\n
+    You are a capable AI model that answers the question as detailed and effectively as possible from the provided context.\n
+    Your answer should be as close to the provided context as possible.\n
+    Give as many results as specified by the user by focusing on quantifying words. If user does not specify, give a concise answer.\n
+    Make sure to provide all the details, if the answer is not in provided context just tell the user that you could not find the relevant answer in the given context, don't provide the wrong answer.\n\n
 
-    Context:\n {context}?\n
-    Question: \n{question}\n
+    Context: {context}\n
+    Question: {question}\n
 
     Answer:
     """
 
-    model = ChatGoogleGenerativeAI(model = "gemini-pro", temperatue = 0.0) # create object of gemini-pro
+    model = ChatGoogleGenerativeAI(model = "gemini-1.5-pro", temperatue = 0.0) # create object of gemini-pro
 
     prompt = PromptTemplate(template = prompt_template, input_variables= ["context","question"])
 
-    chain = load_qa_chain(model,chain_type="stuff",prompt = prompt)
+    #vec_store = get_vector_store()
+    chain = load_qa_chain(model, chain_type="stuff", prompt = prompt)
 
     return chain
 
@@ -71,12 +77,13 @@ def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/text-embedding-004")
     # load the local faiss db
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-
+    retriever = new_db.as_retriever(search_type = 'mmr', search_kwargs={"k": 3})
     # using similarity search, get the answer based on the input
-    docs = new_db.similarity_search(user_question)
-    chain = get_conversation_chain()
+    # docs = new_db.similarity_search(user_question)
 
-    
+    chain = get_conversation_chain(retriever)   
+    docs = retriever.invoke(user_question)
+
     response = chain(
         {"input_documents":docs, "question": user_question}
         , return_only_outputs=True)
@@ -90,25 +97,32 @@ def main():
     st.header("Chat with your PDF Document!")
     with st.container():
         pdf_docs = st.file_uploader("", accept_multiple_files=True)
-        if st.button("Submit", type = "primary"):
-            with st.spinner("Processing..."):
-                raw_text = get_pdf_text(pdf_docs)
-                text_chunks = get_text_chunks(raw_text)
-                get_vector_store(text_chunks)
-                st.success("Done")
+        
+        if pdf_docs:
+            file_name = pdf_docs[0].name
+
+            if file_name.endswith(".pdf"):
+                if st.button("Submit", type = "primary"):
+                    with st.spinner("Processing..."):
+                        raw_text = get_pdf_text(pdf_docs)
+                        text_chunks = get_text_chunks(raw_text)
+                        vec_store = get_vector_store(text_chunks)
+                        # get_conversation_chain()
+                        st.success("Done")
+            else:
+             st.error("Please upload a valid PDF file.")
 
     with st.sidebar:
-        #st.title("Menu")
-        st.markdown("<h1 style='color: red;'>Menu</h1>", unsafe_allow_html=True)
-        st.markdown("<h2 style='color: white;'>Instructions:</h1>", unsafe_allow_html=True)
+        st.markdown("<h2 style='color: red;'>Instruction Menu</h1>", unsafe_allow_html=True)
 
-        #st.header("Instructions: ")
+        st.markdown("<h4 style='color: white;'>1. Upload your PDF Files using the 'Browse Files' button.</h4>", unsafe_allow_html=True)
+        st.markdown("<h4 style='color: white;'>2. Click on the Submit Button</h4>", unsafe_allow_html=True)
+        st.markdown("<h4 style='color: white;'>3. Ask a Question related to your document and get an answer.</h4>", unsafe_allow_html=True)
+        st.markdown("<h4 style='color: white;'>It's just that easy!</h4>", unsafe_allow_html=True)
 
-        st.markdown("<h4 style='color: white;'>1. Upload your PDF Files using the 'Browse Files' button.</h1>", unsafe_allow_html=True)
-        st.markdown("<h4 style='color: white;'>2. Click on the Submit Button", unsafe_allow_html=True)
-        st.markdown("<h4 style='color: white;'>3. Ask a Question related to your document and get an answer.</h1>", unsafe_allow_html=True)
-        st.markdown("<h4 style='color: white;'>It's just that easy!</h1>", unsafe_allow_html=True)
-
+        st.markdown("<h2 style='color: red;'>Points to Note</h1>", unsafe_allow_html=True)
+        st.markdown("<h4 style='color: white;'>1. It is recommended that you fact-check the answers provided by this model.</h4>", unsafe_allow_html=True)
+        st.markdown("<h4 style='color: white;'>2. The information may sometimes be incomplete due to limited output size.</h4>", unsafe_allow_html=True)
 
     user_question = st.text_input("Ask a Question:")
 
@@ -118,3 +132,5 @@ def main():
     
 if __name__ == "__main__":
     main()
+
+
